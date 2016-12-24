7aa59aaaa14741267a47c1c0fbb3ae9266dbfff0
Michael Tran <miketran@Michaels-MacBook-Pro.local>
2016-12-24 14:55:49 -0800

    [feature] - signin app done, need to do validation

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Signin } from "./signin.interface";
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
+
 export class UserLoginComponent implements OnInit {
+  signin: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.signin = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      password: ['', [Validators.required ]],
+    });
   }
-
+  onSubmit({ value, valid }: { value: Signin, valid: boolean }) {
+    console.log(value, valid);
+  }
 }
+
+
