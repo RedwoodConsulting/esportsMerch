6b2176d0175961f35f4c7bbe64074f27fa700310
Michael Tran <miketran@Michaels-MacBook-Pro.local>
2016-12-30 11:04:52 -0800

    [feature] - password added to signup form

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -12,7 +12,6 @@ export class UserLoginComponent implements OnInit {
   signin: FormGroup;
   constructor(private fb: FormBuilder) {}
 
-
   ngOnInit(){
     this.signin = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
